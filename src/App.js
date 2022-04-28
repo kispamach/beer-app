6eb2144be7a45d84fe19a443d773fb9dfb7177a0
Fazekas Dávid <72687550+kispamach@users.noreply.github.com>
2022-04-28 22:12:03 +0200

    Creted main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from './components/Main';
+import './components/style.css'
 
 function App() {
   return (
     <div className="App">
-
+      <Main />
     </div>
   );
 }
